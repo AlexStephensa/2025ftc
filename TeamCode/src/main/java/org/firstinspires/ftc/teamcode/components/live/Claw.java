7ab56881d2159c7a4f2076e1ebe71ac788168d32
Xcentrics <150195241+Xcentrics@users.noreply.github.com>
2024-01-03 16:53:00 -0600

    claw component commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/live/Claw.java
@@ -0,0 +1,115 @@
+package org.firstinspires.ftc.teamcode.components.live;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.components.Component;
+import org.firstinspires.ftc.teamcode.robots.Robot;
+import org.firstinspires.ftc.teamcode.util.qus.ServoQUS;
+
+@Config
+class ClawConfig {
+    public static double CLAW_L_OPEN = 0.39;
+    public static double CLAW_R_OPEN = 0.45;
+    public static double CLAW_L_CLOSE = 0.33;
+    public static double CLAW_R_CLOSE = 0.51;
+    public static double CLAW_PITCH_UP = 0.6;
+    public static double CLAW_PITCH_DOWN = 0.345;
+    public static double MIN_DISTANCE = 10; //mm
+}
+
+public class Claw extends Component {
+
+    public ServoQUS claw_pitch;
+    public ServoQUS claw_l;
+    public ServoQUS claw_r;
+    public Rev2mDistanceSensor left_ds;
+    public Rev2mDistanceSensor right_ds;
+
+    public double left_distance = 1000;
+    public double right_distance = 1000;
+
+    {
+        name = "Claw";
+    }
+
+    public Claw(Robot robot) {
+        super(robot);
+    }
+
+    public void registerHardware (HardwareMap hwmap)
+    {
+        super.registerHardware(hwmap);
+
+        claw_pitch = new ServoQUS(hwmap.get(Servo.class, "claw_pitch"));
+        claw_l = new ServoQUS(hwmap.get(Servo.class, "claw_l"));
+        claw_r = new ServoQUS(hwmap.get(Servo.class, "claw_r"));
+        left_ds = hwmap.get(Rev2mDistanceSensor.class, "left_ds");
+        right_ds = hwmap.get(Rev2mDistanceSensor.class, "right_ds");
+    }
+
+    @Override
+    public void update(OpMode opmode) {
+        super.update(opmode);
+
+        claw_pitch.update();
+        claw_l.update();
+        claw_r.update();
+
+        if (robot.cycle % 5 == 0) {
+            left_distance = left_ds.getDistance(DistanceUnit.MM);
+            right_distance = right_ds.getDistance(DistanceUnit.MM);
+        }
+    }
+
+    @Override
+    public void startup() {
+        super.startup();
+
+        close_left();
+        close_right();
+        pitch_down();
+    }
+
+    @Override
+    public void updateTelemetry(Telemetry telemetry) {
+        super.updateTelemetry(telemetry);
+
+        telemetry.addData("L DIST", TELEMETRY_DECIMAL.format(left_distance));
+        telemetry.addData("R DIST", TELEMETRY_DECIMAL.format(right_distance));
+    }
+
+    public void close_left() {
+        claw_l.queue_position(ClawConfig.CLAW_L_CLOSE);
+    }
+
+    public void close_right() {
+        claw_r.queue_position(ClawConfig.CLAW_R_CLOSE);
+    }
+
+    public void open_left() {
+        claw_l.queue_position(ClawConfig.CLAW_L_OPEN);
+    }
+    public void open_right() {
+        claw_r.queue_position(ClawConfig.CLAW_R_OPEN);
+    }
+    public void pitch_up() {
+        claw_pitch.queue_position(ClawConfig.CLAW_PITCH_UP);
+    }
+    public void pitch_down() {
+        claw_pitch.queue_position(ClawConfig.CLAW_PITCH_DOWN);
+    }
+
+    public boolean left_detected() {
+        return left_distance < ClawConfig.MIN_DISTANCE;
+    }
+
+    public boolean right_detected() {
+        return right_distance < ClawConfig.MIN_DISTANCE;
+    }
+}
