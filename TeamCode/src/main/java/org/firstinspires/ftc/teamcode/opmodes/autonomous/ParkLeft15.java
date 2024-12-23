3a8dcaf231214a7855c6793a4fac4ce508e71853
robotics <robotics5143@crxcs.org>
2024-12-23 11:46:32 -0600

    Auto fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/ParkLeft15.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/ParkLeft15.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode.opmodes.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.opmodes.LiveAutoBase;
+import org.firstinspires.ftc.teamcode.robots.LiveRobot;
 
-@Autonomous(name = "ParkRight", group = "autonomous")
-public class ParkRight extends LiveAutoBase {
+@Autonomous(name = "ParkLeft15", group = "autonomous")
+public class ParkLeft15 extends LiveAutoBase {
 
     @Override
     public void on_init() {
@@ -14,7 +16,7 @@ public class ParkRight extends LiveAutoBase {
     @Override
     public void on_start() {
 
-        robot.drive_train.odo_move(-30,0,0,0.75);
+        robot.drive_train.odo_move(15,0,0,0.75);
 
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/ParkRight15.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/ParkRight15.java
@@ -1,13 +1,11 @@
 package org.firstinspires.ftc.teamcode.opmodes.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.opmodes.LiveAutoBase;
-import org.firstinspires.ftc.teamcode.robots.LiveRobot;
 
-@Autonomous(name = "ParkLeft", group = "autonomous")
-public class ParkLeft extends LiveAutoBase {
+@Autonomous(name = "ParkRight15", group = "autonomous")
+public class ParkRight15 extends LiveAutoBase {
 
     @Override
     public void on_init() {
@@ -16,7 +14,7 @@ public class ParkLeft extends LiveAutoBase {
     @Override
     public void on_start() {
 
-        robot.drive_train.odo_move(30,0,0,0.75);
+        robot.drive_train.odo_move(-15,0,0,0.75);
 
     }
 
