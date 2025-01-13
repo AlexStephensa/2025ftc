1217cb82ee4c2dea567cca8a1bfd984b2f4d8240
Xcentrics <150195241+Xcentrics@users.noreply.github.com>
2025-01-13 17:02:45 -0600

    Auto? i tried

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/Sample1Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/Sample1Auto.java
@@ -2,8 +2,6 @@ package org.firstinspires.ftc.teamcode.opmodes.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-import org.firstinspires.ftc.teamcode.coyote.path.Path;
-import org.firstinspires.ftc.teamcode.coyote.path.PathPoint;
 import org.firstinspires.ftc.teamcode.opmodes.LiveAutoBase;
 
 @Autonomous(name = "1 Sample Auto", group = "autonomous")
@@ -15,24 +13,19 @@ public class Sample1Auto extends LiveAutoBase {
 
     @Override
     public void on_start() {
-        final Path samplePath = new Path();
+
+        /*final Path samplePath = new Path();
+        samplePath.speed(0.5);
         samplePath
-                .addPoint(new PathPoint(20, 20))
-                /*.addPoint(new PathPoint(5, 0).addAction(() -> {
-                    robot.intake.spin(1);
-                    robot.intake.cradle_intake();
-                }))
-                .addPoint(new PathPoint(-4, -12*2).addAction(() -> {
-                    intake_path.speed(0.5);
-                }))
-                .addPoint(new PathPoint(0, -12*4))
-                .positionPrecision(5)
-                .headingPrecision(0.04)
-                .constantHeading(-Math.PI/2)
-                .followRadius(18)
-                .speed(1)
-                .until(() -> robot.intake.has_element())*/;
-                robot.drive_train.follow_curve_path(samplePath);
+                .addPoint(new PathPoint(0, 0))
+                .timeout(5)
+                .addPoint(new PathPoint(2, 0));
+        robot.drive_train.follow_curve_path(samplePath);*/
+
+        robot.drive_train.odo_reset(0.0, 0.0, 0.0);
+        robot.drive_train.odo_move(2.0, 0.0, 0.0, 0.5);
+        robot.wait(500);
+        robot.drive_train.odo_move(2.0, 0.0, 0.0, 0.5);
 
     }
 
